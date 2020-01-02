d5c2a24590d32b9fd006168c6292c3018d59882e
snicmakino <snicmakino@gmail.com>
2020-01-02 17:27:59 +0900

    feat: wip render

---
+++ src/core.rs
@@ -0,0 +1,52 @@
+use serde_json::{json, Value};
+
+pub fn exec() {
+    println!("hello world!!")
+}
+
+pub fn render(text: &str, setting: Value) -> String {
+    let setting_map = setting.as_object();
+    if setting_map.is_none() {
+        return text.to_string();
+    }
+
+    let result = match setting.as_object() {
+        Some(setting_map) => {
+            let mut tmp_text = text.to_string();
+            for (key, value) in setting_map {
+                tmp_text = match value.as_str() {
+                    Some(val) => replace_string(tmp_text, key, val),
+                    None => tmp_text
+                };
+            }
+            return tmp_text;
+        }
+        None => text.to_string(),
+    };
+
+    return result;
+}
+
+fn replace_string(text: String, key: &str, value: &str) -> String {
+    let pattern = &(["{{", key, "}}"].concat());
+    return text.replace(pattern, value);
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::{json, Value};
+
+    use super::render;
+
+    #[test]
+    fn test_render_no_setting() {
+        let setting = json!("{}");
+        assert_eq!("hello rtpl!!".to_string(), render("hello rtpl!!", setting))
+    }
+
+    #[test]
+    fn test_render_only_one_word() {
+        let setting = json!({"name": "rtpl"});
+        assert_eq!("hello rtpl!!".to_string(), render("hello {{name}}!!", setting));
+    }
+}
